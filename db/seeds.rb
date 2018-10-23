f709dea773e99a59fa9516c798ea5d9990d234a4
McKenzie Fiege <mckenziefiege@McKenzies-MacBook-Pro.local>
2018-10-23 10:34:21 -0400

    fixed seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,20 +11,20 @@ Prompt.destroy_all
 jackie = User.create(username: 'jkhaha')
 mckenzie = User.create(username: 'fiegelikesiege')
 
-sonnet1 = Prompt.create(content: "From fairest creatures we desire increase,
-That thereby beauty's rose might never die,
-But as the riper should by time decease,
-His tender heir might bear his memory:
-But thou contracted to thine own bright eyes,
-Feed'st thy light's flame with self-substantial fuel,
-Making a famine where abundance lies,
-Thy self thy foe, to thy sweet self too cruel:
-Thou that art now the world's fresh ornament,
-And only herald to the gaudy spring,
-Within thine own bud buriest thy content,
-And tender churl mak'st waste in niggarding:
-  Pity the world, or else this glutton be,
-  To eat the world's due, by the grave and thee.")
+sonnet1 = Prompt.create(content: "Those hours, that with gentle work did frame
+The lovely gaze where every eye doth dwell,
+Will play the tyrants to the very same
+And that unfair which fairly doth excel;
+For never-resting time leads summer on
+To hideous winter, and confounds him there;
+Sap checked with frost, and lusty leaves quite gone,
+Beauty o'er-snowed and bareness every where:
+Then were not summer's distillation left,
+A liquid prisoner pent in walls of glass,
+Beauty's effect with beauty were bereft,
+Nor it, nor no remembrance what it was:
+  But flowers distill'd, though they with winter meet,
+  Leese but their show; their substance still lives sweet.")
 
 sonnet2 = Prompt.create(content: "When forty winters shall besiege thy brow,
 And dig deep trenches in thy beauty's field,
