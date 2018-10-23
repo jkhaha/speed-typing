c2b883b97ddb1c62924e0883ea73135bb7353603
jkhaha <jha2@wellesley.edu>
2018-10-23 11:35:34 -0400

    new prompts

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,32 +11,22 @@ Prompt.destroy_all
 jackie = User.create(username: 'jkhaha')
 mckenzie = User.create(username: 'fiegelikesiege')
 
-sonnet1 = Prompt.create(content: "Those hours, that with gentle work did frame
-The lovely gaze where every eye doth dwell,
-Will play the tyrants to the very same
-And that unfair which fairly doth excel;
-For never-resting time leads summer on
-To hideous winter, and confounds him there;
-Sap checked with frost, and lusty leaves quite gone,
-Beauty o'er-snowed and bareness every where:
-Then were not summer's distillation left,
-A liquid prisoner pent in walls of glass,
-Beauty's effect with beauty were bereft,
-Nor it, nor no remembrance what it was:
-  But flowers distill'd, though they with winter meet,
-  Leese but their show; their substance still lives sweet.")
+prompt1 = Prompt.create(content: "Fans, for the past two weeks you have been reading about a bad break I got. Yet today I consider myself the luckiest man on the face of the earth. I have been in ballparks for seventeen years and have never received anything but kindness and encouragement from you fans.
 
-sonnet2 = Prompt.create(content: "When forty winters shall besiege thy brow,
-And dig deep trenches in thy beauty's field,
-Thy youth's proud livery so gazed on now,
-Will be a tatter'd weed of small worth held:
-Then being asked, where all thy beauty lies,
-Where all the treasure of thy lusty days;
-To say, within thine own deep sunken eyes,
-Were an all-eating shame, and thriftless praise.
-How much more praise deserv'd thy beauty's use,
-If thou couldst answer 'This fair child of mine
-Shall sum my count, and make my old excuse,'
-Proving his beauty by succession thine!
-  This were to be new made when thou art old,
-  And see thy blood warm when thou feel'st it cold.")
+Look at these grand men. Which of you wouldn’t consider it the highlight of his career to associate with them for even one day!
+
+Sure, I’m lucky. Who wouldn’t consider it an honor to have known Jacob Ruppert – also the builder of baseball’s greatest empire, Ed Barrow – to have spent the next nine years with that wonderful little fellow Miller Huggins – then to have spent the next nine years with that outstanding leader, that smart student of psychology – the best manager in baseball today, Joe McCarthy!
+
+Sure, I’m lucky. When the New York Giants, a team you would give your right arm to beat, and vice versa, sends you a gift, that’s something! When everybody down to the groundskeepers and those boys in white coats remember you with trophies, that’s something.
+
+When you have a wonderful mother-in-law who takes sides with you in squabbles against her own daughter, that’s something. When you have a father and mother who work all their lives so that you can have an education and build your body, it’s a blessing! When you have a wife who has been a tower of strength and shown more courage than you dreamed existed, that’s the finest I know.
+
+So I close in saying that I might have had a tough break – but I have an awful lot to live for!")
+
+prompt2 = Prompt.create(content: "I would like to thank the Secretary General for inviting me to be part of this important United Nations Fourth World Conference on Women. This is truly a celebration, a celebration of the contributions women make in every aspect of life: in the home, on the job, in the community, as mothers, wives, sisters, daughters, learners, workers, citizens, and leaders.
+
+It is also a coming together, much the way women come together every day in every country. We come together in fields and factories, in village markets and supermarkets, in living rooms and board rooms. Whether it is while playing with our children in the park, or washing clothes in a river, or taking a break at the office water cooler, we come together and talk about our aspirations and concern. And time and again, our talk turns to our children and our families. However different we may appear, there is far more that unites us than divides us. We share a common future, and we are here to find common ground so that we may help bring new dignity and respect to women and girls all over the world, and in so doing bring new strength and stability to families as well.
+
+By gathering in Beijing, we are focusing world attention on issues that matter most in our lives -- the lives of women and their families: access to education, health care, jobs and credit, the chance to enjoy basic legal and human rights and to participate fully in the political life of our countries.
+
+There are some who question the reason for this conference. Let them listen to the voices of women in their homes, neighborhoods, and workplaces. There are some who wonder whether the lives of women and girls matter to economic and political progress around the globe. Let them look at the women gathered here and at Huairou -- the homemakers and nurses, the teachers and lawyers, the policymakers and women who run their own businesses. It is conferences like this that compel governments and peoples everywhere to listen, look, and face the world’s most pressing problems.")
