928f72b0edd108d9587d0dd9e85711ea01f25f5e
jkhaha <jha2@wellesley.edu>
2018-10-24 15:19:59 -0400

    wednesday at 3:19pm

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,13 +15,7 @@ class UsersController < ApplicationController
 
   # POST /users
   def create
-    @user = User.new(user_params)
-
-    if @user.save
-      render json: @user, status: :created, location: @user
-    else
-      render json: @user.errors, status: :unprocessable_entity
-    end
+    @user = User.find_or_create_by(user_params)
   end
 
   # PATCH/PUT /users/1
